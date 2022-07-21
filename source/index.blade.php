0198a683496a39aec812eae903be8b3d21840240
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-07-21 18:38:57 +0900

    save

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('body')
 <div class="p-8">
-    <h1 class="text-3xl font-bold">Hello world!</h1>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </div>
 @endsection
