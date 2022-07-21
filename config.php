8a9d1c54828afd10dcbb9d11dfe25bc1d5f09c7b
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-07-21 17:35:15 +0900

    setting public folder

--- config.php
+++ config.php
@@ -3,7 +3,10 @@
 return [
     'production' => false,
     'baseUrl' => '',
-    'title' => 'Jigsaw',
-    'description' => 'Website description.',
+    'title' => 'Tiptap 非公式日本語ドキュメント',
+    'description' => 'Tiptapの非公式日本語ドキュメントです',
     'collections' => [],
+    'build' => [
+        'destination' => 'public',
+    ],
 ];
